88f70f5434e4f5b5488f13dd5984b5845dc42ae3
Alex Lord <alex.lord9@hotmail.co.uk>
2018-10-13 22:46:17 +0100

    Started Menu page. Footer mobile-friendly.  New home buttons.  Fixed navbar.

---
+++ scripts/navbar.js
@@ -0,0 +1,18 @@
+
+// Change navbar background colour and font colour
+// Once navbar gets to FOOD section
+$(document).ready(function(){
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+
+        if (scroll > 900) {
+            $(".header").css("background" , "#FFF");
+            $(".header a.enabled").css("color" , "#000");
+        }
+
+        else{
+            $(".header").css("background" , "transparent");
+            $(".header a.enabled").css("color" , "#FFF");
+        }
+    })
+})
