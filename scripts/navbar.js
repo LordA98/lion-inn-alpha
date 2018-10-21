3f8e84b31e2651f8b55599fc8f46d2313d0a664f
Alex Lord <alex.lord9@hotmail.co.uk>
2018-10-21 18:49:26 +0100

    Fixing scaling issues.

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,17 +1,36 @@
-// Change navbar background colour and font colour
-// Once navbar gets to FOOD section
+
 $(document).ready(function(){
+    var lion_purple = "#673ab7";
+
+    // Change navbar background colour, font colour and hamburger icon
+    // Once navbar gets to FOOD section
     $(window).scroll(function(){
         var scroll = $(window).scrollTop();
 
         if (scroll > 800) {
             $(".header").css("background" , "#FFF");
             $(".header a.enabled").css("color" , "#000");
+            $(".navbar-toggler i").css("color" , lion_purple);
         }
 
-        else{
+        else {
             $(".header").css("background" , "transparent");
             $(".header a.enabled").css("color" , "#FFF");
+            $(".navbar-toggler i").css("color" , "#FFF");
         }
-    })
+    });
+
+    // On hamburger click
+    // Change header section (where logo and hamburger is)
+    $('#hamburger').click(function() {
+        // Toggle hamburger colour
+        $(".navbar-toggler i").toggleClass("purple-hamburger");
+        // Toggle background colour
+        $("#navbar-main").toggleClass("clicked");
+    });
+
+    // Close navbar when a nav element link is clicked
+    $(".navbar-nav li a").click(function(event) {
+        $(".navbar-collapse").collapse('hide');
+    });
 })
